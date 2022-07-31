57968770dfa2b8c5fb81622c663c9c0dea851ad6
Rao <siddharr@f4d48866e200.ant.amazon.com>
2022-07-31 13:24:22 -0700

    Initial commit

---
+++ test/sma-contact-flow-stack.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SmaContactFlowStack from '../lib/sma-contact-flow-stack-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/sma-contact-flow-stack-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SmaContactFlowStack.SmaContactFlowStackStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
